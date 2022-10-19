ec14980ddfe51c913f29a6c6c2b86aab5a07d9d3
Daza <williamdaza04@gmail.com>
2022-10-19 15:13:06 -0500

    Se crea servidor

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+const admin = require("firebase-admin");
+const credentials = require("./gestion-de-inventario-f637f-firebase-adminsdk-rbupd-e99f044b86.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+});
+
+const db = admin.firestore();
+
+app.use(express.json());
+
+app.user(express.urlencoded({extended: true}));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
