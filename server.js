c31fdb2d99d5349711fd3f2ae2f0b544e3cfa80e
Daza <williamdaza04@gmail.com>
2022-10-20 21:15:57 -0500

    Se hacen ajustes al servidor y algunas peticiones

--- server.js
+++ server.js
@@ -1,18 +1,105 @@
+const { Router } = require("express");
 const express = require("express");
 const app = express();
+const router = Router();
 
 const admin = require("firebase-admin");
-const credentials = require("./gestion-de-inventario-f637f-firebase-adminsdk-rbupd-e99f044b86.json");
+const credentials = require("./firebase.json");
 
+//Credenciales de db
 admin.initializeApp({
     credential: admin.credential.cert(credentials)
 });
 
+//Middleware
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//Inicializacion de bd
 const db = admin.firestore();
 
-app.use(express.json());
+//Peticiones
+//Sede
+//Get
+app.get('/sedes', async (req, res) => {
+    const getSede = await db.collection("Sedes").get();
+    const sede = getSede.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }))
+    res.send(sede)
+});
 
-app.use(express.urlencoded({extended: true}));
+//Post
+app.post('/sedes', async(req, res) => {
+    const data = req.body;
+    const postSede = await db.collection("Sedes").add(data);
+    res.send("Agregado con exito");
+});
+
+//Delete
+app.delete('/sedes/:id', async(req, res) => {
+    const delSede = await db.collection("Sedes").doc(req.params.id).delete();   
+    res.send("Eliminado con exito");
+})
+
+//Put
+app.put('/sedes/:id', async(req, res) => {
+    console.log(req.body);
+    const uptSede = await db.collection("Sedes").doc(req.params.id).update(req.body);   
+    res.send("Actualizado con exito");
+})
+
+//Repuestos
+//Get
+app.get('/repuestos', async (req, res) => {
+    const getSede = await db.collection("Repuestos").get();
+    const sede = getSede.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+    }))
+    res.send(sede)
+});
+
+//Post
+app.post('/repuestos', async(req, res) => {
+    const data = req.body;
+    const postSede = await db.collection("Repuestos").add(data);
+    res.send("Agregado con exito");
+});
+
+//Delete
+app.delete('/repuestos/:id', async(req, res) => {
+    const delSede = await db.collection("Repuestos").doc(req.params.id).delete();   
+    res.send("Eliminado con exito");
+})
+
+//Put
+app.put('/repuestos/:id', async(req, res) => {
+    console.log(req.body);
+    const uptSede = await db.collection("Repuestos").doc(req.params.id).update(req.body);   
+    res.send("Actualizado con exito");
+})
+
+
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
